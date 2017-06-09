e79433d03de5177710bc03bd74b20a01f1a390e7
Geoffrey M. Sametz <sametz@udel.edu>
2017-06-08 21:29:33 -0400

    FT will run management commands on server.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -24,9 +24,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            setattr(self, 'live_server_url', 'http://' + self.staging_server)
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
