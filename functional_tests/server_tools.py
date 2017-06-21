a803dcce82dd32f2259f781dbac000cde0334498
Geoffrey M. Sametz <sametz@udel.edu>
2017-06-20 21:29:42 -0400

    FT will run management commands on server.

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,5 +1,8 @@
-from fabric.api import run
+from fabric.api import env, run
 from fabric.context_managers import settings
+
+# OTTG answer to my Q in Ch21: fix to get around AWS ubuntu password request
+env.key_filename = ["~/Documents/aws/thompetz_aws.pem"]
 
 
 def _get_manage_dot_py(host):
@@ -9,6 +12,8 @@ def _get_manage_dot_py(host):
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'ubuntu@{host}'):
+        # TDD version (line below) required ubuntu password, not available
+        # on standard AWS EC2
         run(f'{manage_dot_py} flush --noinput')
 
 
